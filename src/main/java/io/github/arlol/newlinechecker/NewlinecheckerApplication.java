908f13d0495b04f8efcb4c52a3e161f28ecd518e
Arlo O'Keeffe <a.okeeffe@sprinteins.com>
2020-09-28 18:56:00 +0200

    Refactor NewlinecheckerApplication

--- src/main/java/io/github/arlol/newlinechecker/NewlinecheckerApplication.java
+++ src/main/java/io/github/arlol/newlinechecker/NewlinecheckerApplication.java
@@ -10,6 +10,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.eclipse.jgit.lib.ObjectId;
 import org.eclipse.jgit.lib.Repository;
@@ -20,87 +21,87 @@ import org.eclipse.jgit.treewalk.TreeWalk;
 
 public class NewlinecheckerApplication {
 
-    public static void main(String[] args) throws IOException, InterruptedException {
-        jgit().stream()
-                .filter(NewlinecheckerApplication::filterByFilename)
-                .map(File::new)
-                .forEach(NewlinecheckerApplication::checkIfNewlineAtEof);
-        git().stream()
-                .filter(NewlinecheckerApplication::filterByFilename)
-                .map(File::new)
-                .forEach(NewlinecheckerApplication::checkIfNewlineAtEof);
-    }
+	public static void main(String[] args)
+			throws IOException, InterruptedException {
+		Stream.concat(jgit().stream(), git().stream())
+				.filter(NewlinecheckerApplication::filterByFilename)
+				.distinct()
+				.map(File::new)
+				.filter(NewlinecheckerApplication::checkIfNewlineAtEof)
+				.forEach(file -> {
+					System.out
+							.println(file + " does not have a newline at EOF");
+				});
+	}
 
-    private static List<String> jgit() throws IOException {
-        List<String> result = new ArrayList<>();
-        try (Repository repository = new FileRepositoryBuilder()
-                .setMustExist(true)
-                .readEnvironment()
-                .findGitDir()
-                .build();
-             RevWalk revWalk = new RevWalk(repository);
-             TreeWalk treeWalk = new TreeWalk(repository)) {
-            ObjectId headId = repository.resolve("HEAD");
-            RevCommit headCommit = revWalk.parseCommit(headId);
-            treeWalk.addTree(headCommit.getTree());
-            while (treeWalk.next()) {
-                if (treeWalk.isSubtree()) {
-                    treeWalk.enterSubtree();
-                } else {
-                    result.add(treeWalk.getPathString());
-                }
-            }
-        }
-        return result;
-    }
+	private static List<String> jgit() throws IOException {
+		List<String> result = new ArrayList<>();
+		try (Repository repository = new FileRepositoryBuilder()
+				.setMustExist(true)
+				.readEnvironment()
+				.findGitDir()
+				.build();
+				RevWalk revWalk = new RevWalk(repository);
+				TreeWalk treeWalk = new TreeWalk(repository)) {
+			ObjectId headId = repository.resolve("HEAD");
+			RevCommit headCommit = revWalk.parseCommit(headId);
+			treeWalk.addTree(headCommit.getTree());
+			while (treeWalk.next()) {
+				if (treeWalk.isSubtree()) {
+					treeWalk.enterSubtree();
+				} else {
+					result.add(treeWalk.getPathString());
+				}
+			}
+		}
+		return result;
+	}
 
-    private static List<String> git() throws IOException, InterruptedException {
-        Process process = new ProcessBuilder()
-                .command(Arrays.asList("git", "grep", "-I", "--files-with-matches", "\"\""))
-                .start();
-        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
-            return bufferedReader
-                    .lines()
-                    .collect(Collectors.toList());
-        } finally {
-            process.waitFor();
-        }
-    }
+	private static List<String> git() throws IOException, InterruptedException {
+		Process process = new ProcessBuilder().command(Arrays
+				.asList("git", "grep", "-I", "--files-with-matches", "\"\""))
+				.start();
+		try (BufferedReader bufferedReader = new BufferedReader(
+				new InputStreamReader(process.getInputStream()))) {
+			return bufferedReader.lines().collect(Collectors.toList());
+		} finally {
+			process.waitFor();
+		}
+	}
 
-    private static boolean filterByFilename(String filename) {
-        return !filename.contains(".idea/");
-    }
+	private static boolean filterByFilename(String filename) {
+		return !filename.contains(".idea/");
+	}
 
-    private static void checkIfNewlineAtEof(File file) {
-        try (RandomAccessFile randomAccessFile = new RandomAccessFile(file, "r")) {
-            if (fileIsEmptyOrBinary(randomAccessFile)) {
-                return;
-            }
-            randomAccessFile.seek(file.length() - 1);
-            if (randomAccessFile.read() != 10) {
-                System.out.println(file + " does not have a newline at EOF");
-                System.exit(1);
-            }
-        } catch (IOException e) {
-            throw new UncheckedIOException(e);
-        }
-    }
+	public static boolean checkIfNewlineAtEof(File file) {
+		try (RandomAccessFile randomAccessFile = new RandomAccessFile(file,
+				"r")) {
+			if (fileIsEmptyOrBinary(randomAccessFile)) {
+				return false;
+			}
+			randomAccessFile.seek(file.length() - 1);
+			return randomAccessFile.read() != 10;
+		} catch (IOException e) {
+			throw new UncheckedIOException(e);
+		}
+	}
 
-    private static boolean fileIsEmptyOrBinary(RandomAccessFile randomAccessFile) throws IOException {
-        byte[] bytes = new byte[8000];
-        randomAccessFile.seek(0);
-        int readResult = randomAccessFile.read(bytes, 0, 8000);
-        if (readResult == -1) {
-            // empty file
-            return true;
-        }
-        for (int i = 0; i < readResult; i++) {
-            if (bytes[i] == 0) {
-                // binary file
-                return true;
-            }
-        }
-        return false;
-    }
+	public static boolean fileIsEmptyOrBinary(RandomAccessFile randomAccessFile)
+			throws IOException {
+		byte[] bytes = new byte[8000];
+		randomAccessFile.seek(0);
+		int readResult = randomAccessFile.read(bytes, 0, 8000);
+		if (readResult == -1) {
+			// empty file
+			return true;
+		}
+		for (int i = 0; i < readResult; i++) {
+			if (bytes[i] == 0) {
+				// binary file
+				return true;
+			}
+		}
+		return false;
+	}
 
 }
