5e57468762121d50e97fb00bdec3a2c3258e7881
Arlo O'Keeffe <tiger@k5d.de>
2022-12-01 21:51:11 +0100

    refactor version output

--- src/main/java/io/github/arlol/newlinechecker/NewlinecheckerApplication.java
+++ src/main/java/io/github/arlol/newlinechecker/NewlinecheckerApplication.java
@@ -28,10 +28,9 @@ public class NewlinecheckerApplication {
 
 	public static void main(String[] args) {
 		if (args.length == 1 && "--version".equals(args[0])) {
-			String title = NewlinecheckerApplication.class.getPackage()
-					.getImplementationTitle();
-			String version = NewlinecheckerApplication.class.getPackage()
-					.getImplementationVersion();
+			Package pkg = NewlinecheckerApplication.class.getPackage();
+			String title = pkg.getImplementationTitle();
+			String version = pkg.getImplementationVersion();
 			System.out.println(title + " version \"" + version + "\"");
 			return;
 		}
